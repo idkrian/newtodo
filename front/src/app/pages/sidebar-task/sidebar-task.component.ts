5fd28999bd9d1227320f9941ab0b5efce7200c35
Rian <riansouza0@gmail.com>
2024-03-06 21:09:24 -0300

    feat: sidebar task options

--- front/src/app/pages/sidebar-task/sidebar-task.component.ts
+++ front/src/app/pages/sidebar-task/sidebar-task.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { DataService } from '../../data.service';
 import { CommonModule } from '@angular/common';
+import { AddTaskButtonComponent } from '../../components/molecules/add-task-button/add-task-button.component';
+import { TaskComponent } from '../../components/molecules/task/task.component';
+import { ChipModule } from 'primeng/chip';
 
 @Component({
   selector: 'app-sidebar-task',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AddTaskButtonComponent, TaskComponent, ChipModule],
   templateUrl: './sidebar-task.component.html',
 })
 export class SidebarTaskComponent {
